480dca9e608b1729a306e629cdcdf6ada73073c1
Ted <54852276+Niarb144@users.noreply.github.com>
2020-07-16 22:29:28 +0300

    Update lab1.php

--- iap lab1/lab1.php
+++ iap lab1/lab1.php
@@ -8,6 +8,11 @@ if (isset($_POST['btn-save'])){
 	$city = $_POST['city_name'];
 
 	$user = new User($first_name, $last_name, $city);
+	if (!$user -> validateForm()){
+		$user -> createFormErrorSessions ();
+		header ("Refresh:0");
+		die();
+	}
 	$sql = $user ->save();
 
 	if($sql){
@@ -15,15 +20,31 @@ if (isset($_POST['btn-save'])){
 	}else{
 		echo "An error ocurred!";
 	}
+	$conn -> closeDatabase();
 }?>
 <html>
 <head>
 	<title>User</title>
+	<script type ="text/javascript" src = "validate.js"></script>
+	<link rel = "stylesheet" type = "text/css" href ="validate.css">
 
 </head>
 <body>
-	<form method = "post">
+	<form method = "post" name = "user_details" id = "user_details" onsubmit = "return validateForm()" action = "<? = $_SERVER['PHP_SELF']?>">
 		<table align = "center">
+			<tr>
+				<td>
+					<div id = "form-errors">
+						<?php
+						session_start();
+						if (!empty ($_SESSION['form_errors'])){
+							echo " " . $_SESSION ['form_errors'];
+							unset ($_SESSION['form_errors']);
+						}
+						?>
+					</div>
+				</td>
+			</tr>
 			<tr>
 				<td><input type = "text" name = "first_name" required placeholder = "First Name"/></td>
 			</tr>
@@ -34,7 +55,16 @@ if (isset($_POST['btn-save'])){
 				<td><input type = "text" name = "city_name" placeholder = "City"/></td>
 			</tr>
 			<tr>
+				<td><input type = "text" name = "username" placeholder = "Username"/></td>
+			</tr>
+			<tr>
+				<td><input type = "password" name = "password" placeholder = "Password"/></td>
+			</tr>
+			<tr>
 				<td><button type = "submit" name = "btn-save"><strong> SAVE</strong></button></td>
+			</tr>
+			<tr>
+				<td><a href = "login.php">Login</a></td>
 			</tr>
 		</table>
 	</form>
